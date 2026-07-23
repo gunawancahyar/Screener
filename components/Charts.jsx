a48335bcf883e5a20d8b3089c52460eed2a5b4ff
gunawancahyar <146130153+gunawancahyar@users.noreply.github.com>
2026-07-24 00:47:01 +0700

    Add files via upload

---
+++ components/Charts.jsx
@@ -0,0 +1,8 @@
+// components/Charts.jsx
+'use client';
+
+export {
+  ResponsiveContainer, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis,
+  Radar, PieChart, Pie, Cell, BarChart, Bar, XAxis, YAxis, CartesianGrid,
+  Tooltip, RadialBarChart, RadialBar, LineChart, Line, Legend
+} from "recharts";
